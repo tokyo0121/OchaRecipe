af7651d8bd0433437dc467c94cc8310b26a2ac4e
Saki Igeta <p19026is@gm.tsuda.ac.jp>
2021-05-26 00:05:41 +0900

    ロゴの反映

--- OchaRecipe/ViewController.swift
+++ OchaRecipe/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //セグメントのアウトレット
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    //ラベルのアウトレット
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func segmentChanged(sender: AnyObject){
+        //セグメントが変更されたときの処理
+        //選択されているセグメントのインデックス
+        let selectedIndex = segmentedControl.selectedSegmentIndex
+        //選択されたインデックスの文字列を取得してラベルに設定
+        label.text = segmentedControl.titleForSegment(at: selectedIndex)
+    }
 
 }
 
